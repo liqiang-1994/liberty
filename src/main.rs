2d445bffa4932f721ab4b74eed1eb011045f4aca
luxyva <luxyva@outlook.com>
2020-12-03 21:47:53 +0800

    actix-web quick start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use actix_web::{get, web, Responder, HttpServer, App};
+#[get("/{id}/{name}")]
+async fn index(web::Path((id, name)):web::Path<(u32, String)>) -> impl Responder {
+    format!("hello {} id:{}", id, name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     println!("Hello, world!");
+    HttpServer::new(||  App::new().service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
